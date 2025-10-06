e6d42ec32bddc272bf3ed64ce5a6db21ea29802e
GabrielRibeiro <ogabrielribeiro97@gmail.com>
2025-10-06 09:30:44 -0300

    feat: Adding Validation whith lib FluentValidation from asp net core

---
+++ DevFreela.Application/Validators/CreateUserValidator.cs
@@ -0,0 +1,20 @@
+﻿using DevFreela.Application.Commands.InsertUser;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using System.Data;
+
+namespace DevFreela.Application.Validators;
+
+public class CreateUserValidator : AbstractValidator<InsertUserCommand>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(c => c.Email)
+            .EmailAddress()
+            .WithMessage("O email é inválido.");
+
+        RuleFor(u => u.BirthDate)
+            .Must(d => d < DateTime.Now.AddYears(-18))
+            .WithMessage("O usuário deve ter pelo menos 18 anos.");
+    }
+}
